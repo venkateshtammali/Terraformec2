4a5c2135b921f62348cb5aab0a5f76efc6e7edc8
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-10-21 12:56:52 +0530

    Update test3.tf

--- test3.tf
+++ test3.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.a}"
-  secret_key = "${var.b}"
-  region     = "ap-south-1"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+  region     = "${var.AWS_REGION}"
 }
 
 resource "aws_eip_association" "eip_assoc" {
